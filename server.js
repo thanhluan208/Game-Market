9ace3973dc407c812479f2c24dfa8bd4ebb74fc5
thanhluan208 <skthunte33@gmail.com>
2022-08-15 09:21:50 +0700

    update database

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const jsonServer = require("json-server");
+const jsonServerAuth = require("json-server-auth");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+
+server.db = router.db;
 
 const PORT = process.env.PORT || 3004;
 
-server.use(middlewares);
+server.use(jsonServerAuth)
 server.use(router);
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
