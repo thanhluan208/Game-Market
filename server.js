914ce5ba8c63364c45ee3e2a78783646c685c1b1
thanhluan208 <skthunte33@gmail.com>
2022-08-15 09:36:24 +0700

    update server

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const jsonServer = require("json-server");
-const jsonServerAuth = require("json-server-auth");
-
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-
 const PORT = process.env.PORT || 3004;
 
-server.use(jsonServerAuth)
 server.use(middlewares);
 server.use(router);
 server.listen(PORT, () => {
